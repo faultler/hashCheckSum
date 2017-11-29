01784d231f04d324a7648b211055d94a92e44f0a
faultler <31318318+faultler@users.noreply.github.com>
2017-11-28 22:49:31 -0500

    Update hashCheckSum.py

--- hashCheckSum.py
+++ hashCheckSum.py
@@ -11,9 +11,9 @@ It will then find the only .md5 text file used in that directory using the Pytho
 the function will return a single string that will later be used in a MS comment prompt
 '''
 def createCommandLine(subdirectoryPath):
-	tempStr = subdirectoryPath + "\*.md5"	#It is assumbed that there is only one .md5 file in the subdirectory
-	textFilePath = glob.glob(subdirectoryPath + ”\*.md5”)
-	stringToReturn = r”md5deep -r -x “ + textFilePath + r” -e “ + subdirectoryPath
+	tempStr = subdirectoryPath + "\*.md5"	#It is assumed that there is only one .md5 file in the subdirectory
+	textFilePath = glob.glob(tempStr)[0]	#glob.glob creates list of all .md5 file paths. since there is only one file, I am taking the first element of the list [0]
+	stringToReturn = r"md5deep -r -x " + textFilePath + r" -e " + subdirectoryPath		# spaces in quotes are necessary
 	return stringToReturn
 
 ############################################################
@@ -31,16 +31,27 @@ def runCommand(commandLine):
 '''
 this section will take input from the user regarding directory paths, and text file naming
 '''
-directorInput = input(“Hey buddy! Enter the path to the main directory with the 24 subdirectories: \n”)
-print(“\n”)
+directorInput = input("Hey buddy! Enter the path to the main directory with the 24 subdirectories: \n")
+print("\n")
 
 ############################################################
 '''
 create list of all subdirectories
-'''
-createSubdirectories(mainDirectory):
-	
+example output:
 
+['C:\\Users\\userName\\Desktop\\testing\\FolderA',
+ 'C:\\Users\\userName\\Desktop\\testing\\FolderB',
+ 'C:\\Users\\userName\\Desktop\\testing\\FolderC']
+'''
+def createSubdirectories(mainDirectory):
+    listOfSubdirectoryNames = []
+    for (dirpath, dirnames, filenames) in os.walk(mainDirectory):
+        listOfSubdirectoryNames.extend(dirnames)
+        break
+    for count in range(len(listOfSubdirectoryNames)):
+        tempVar = mainDirectory + "\\"
+        listOfSubdirectoryNames[count] = tempVar  + listOfSubdirectoryNames[count]
+    return listOfSubdirectoryNames
 
 ############################################################
 '''
